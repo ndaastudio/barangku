64b3cfec1853edb1688a199d9cd51c8d74621803
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-08 16:56:15 +0700

    created list jasa page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,56 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController,
+    private router: Router) { }
 
+  async showKategori() {
+    const alert = await this.alertCtrl.create({
+      header: 'Pilih Kategori',
+      inputs: [
+        {
+          name: 'kategori',
+          type: 'radio',
+          label: 'Jasa Pendidikan',
+          value: 'Jasa Pendidikan',
+          checked: false
+        },
+        {
+          name: 'kategori',
+          type: 'radio',
+          label: 'Jasa Kesehatan',
+          value: 'Jasa Kesehatan',
+          checked: false
+        },
+        {
+          name: 'kategori',
+          type: 'radio',
+          label: 'Jasa Kebersihan',
+          value: 'Jasa Kebersihan',
+          checked: false
+        },
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+          role: 'cancel',
+        },
+        {
+          text: 'Pilih',
+          handler: (data) => {
+            console.log(data);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  goToTambahJasa() {
+    this.router.navigateByUrl('/tambah-jasa');
+  }
+
+  goToShowJasa() {
+    this.router.navigateByUrl('/show-jasa');
+  }
 }
