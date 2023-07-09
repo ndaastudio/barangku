d639bc474b768070185fffbd51a408b4ea6c4690
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-10 00:52:38 +0700

    created logic for register

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
+import { APIService } from 'src/services/API/api.service';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +10,18 @@ import { AlertController, LoadingController } from '@ionic/angular';
 })
 export class RegisterPage implements OnInit {
   nama: any;
+  email: any;
   nomor_telepon: any;
   password: any;
   konfirmasi_password: any;
   kode_daftar: any;
+  setuju: any;
   isValidKodeDaftar: boolean = false;
 
   constructor(private loadingCtrl: LoadingController,
-    private alertCtrl: AlertController) {
+    private alertCtrl: AlertController,
+    private apiService: APIService,
+    private router: Router) {
   }
 
   async showLoading(message: string) {
@@ -25,13 +31,20 @@ export class RegisterPage implements OnInit {
     await loading.present();
   }
 
-  async showAlert(header: string, message: string) {
-    const alert = await this.alertCtrl.create({
-      header: header,
-      message: message,
-      buttons: ['OK']
+  async showAlert(header: string, message: string): Promise<void> {
+    return new Promise<void>(async (resolve) => {
+      const alert = await this.alertCtrl.create({
+        header: header,
+        message: message,
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            resolve();
+          }
+        }]
+      });
+      await alert.present();
     });
-    await alert.present();
   }
 
   ngOnInit() {
@@ -39,14 +52,34 @@ export class RegisterPage implements OnInit {
 
   submitDaftar() {
     if (this.nama && this.nomor_telepon && this.password && this.konfirmasi_password) {
-      if (this.password == this.konfirmasi_password) {
-        this.showLoading('Sedang mendaftar...');
-        setTimeout(() => {
-          this.loadingCtrl.dismiss();
-          this.showAlert('Berhasil!', 'Akun telah didaftarkan');
-        }, 1500);
+      if (this.setuju) {
+        if (this.password == this.konfirmasi_password) {
+          this.showLoading('Sedang mendaftar...');
+          const data = {
+            nama: this.nama,
+            email: this.email,
+            nomor_telepon: this.nomor_telepon,
+            password: this.password,
+          };
+          this.apiService.registerAkun(data).then((result: any) => {
+            this.nama = '';
+            this.password = '';
+            this.konfirmasi_password = '';
+            this.setuju = false;
+            this.loadingCtrl.dismiss().then(() => {
+              this.showAlert('Berhasil!', result.message).then(() => {
+                this.router.navigateByUrl('/login');
+              });
+            });
+          }).catch((error: any) => {
+            this.loadingCtrl.dismiss();
+            this.showAlert('Error!', error.error.message);
+          });
+        } else {
+          this.showAlert('Error!', 'Konfirmasi password tidak sama');
+        }
       } else {
-        this.showAlert('Error!', 'Konfirmasi password tidak sama');
+        this.showAlert('Error!', 'Harus menyetujui persyaratan');
       }
     } else {
       this.showAlert('Error!', 'Tidak boleh ada yang kosong');
@@ -56,11 +89,14 @@ export class RegisterPage implements OnInit {
   cekKodeDaftar() {
     if (this.kode_daftar) {
       this.showLoading('Memeriksa kode daftar...');
-      setTimeout(() => {
+      this.apiService.getAkunByKodeDaftar(this.kode_daftar).then((result: any) => {
         this.loadingCtrl.dismiss();
         this.isValidKodeDaftar = true;
-        this.nomor_telepon = '081234567890';
-      }, 1500);
+        this.nomor_telepon = result.data.nomor_telepon;
+      }).catch((error: any) => {
+        this.loadingCtrl.dismiss();
+        this.showAlert('Error!', error.error.message);
+      });
     } else {
       this.showAlert('Error!', 'Tidak boleh ada yang kosong');
     }
