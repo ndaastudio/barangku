fcfe53c07205de70b2fffb2f8297c1e69f99a7e3
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-10 18:30:13 +0700

    show barang is work

---
+++ src/app/barang/show/show.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ShowBarangPage } from './show.page';
+
+describe('ShowBarangPage', () => {
+  let component: ShowBarangPage;
+  let fixture: ComponentFixture<ShowBarangPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(ShowBarangPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/barang/show/show.page.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, PopoverController } from '@ionic/angular';
+import { DataSharingService } from 'src/services/Database/data-sharing.service';
+import { DatabaseService } from 'src/services/Database/database.service';
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.page.html',
+  styleUrls: ['./show.page.scss'],
+})
+export class ShowBarangPage implements OnInit {
+  id: number = this.route.snapshot.params['id'];
+  dataBarang: any = [];
+
+  constructor(private databaseService: DatabaseService,
+    private route: ActivatedRoute,
+    private alertCtrl: AlertController,
+    private dataSharingService: DataSharingService,
+    private router: Router,
+    private popoverCtrl: PopoverController) {
+    this.databaseService.getBarangById(this.id).then((data) => {
+      this.dataBarang = data;
+    });
+  }
+
+  ngOnInit() {
+    this.dataSharingService.refreshedData.subscribe(() => {
+      this.databaseService.getBarangById(this.id).then((data) => {
+        this.dataBarang = data;
+      });
+    });
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  hapusData() {
+    this.popoverCtrl.dismiss();
+    this.alertCtrl.create({
+      header: 'Konfirmasi',
+      message: 'Anda yakin ingin menghapus data ini?',
+      buttons: [
+        {
+          text: 'BATAL',
+          role: 'cancel',
+        },
+        {
+          text: 'YA',
+          handler: () => {
+            this.databaseService.deleteBarangById(this.id).then(() => {
+              this.dataSharingService.refresh();
+              this.router.navigateByUrl('/tabs/tab1');
+            });
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    }
+    );
+  }
+
+  async ubahProgress() {
+    this.popoverCtrl.dismiss();
+    const alert = await this.alertCtrl.create({
+      header: 'Ubah Progress',
+      inputs: [
+        {
+          name: 'progress',
+          type: 'radio',
+          label: 'Belum',
+          value: 0,
+          checked: false
+        },
+        {
+          name: 'progress',
+          type: 'radio',
+          label: 'Selesai',
+          value: 1,
+          checked: false
+        }
+      ],
+      buttons: [
+        {
+          text: 'Batal',
+          role: 'cancel',
+        },
+        {
+          text: 'Pilih',
+          handler: (data) => {
+            this.dataBarang.progress = data;
+            this.databaseService.updateBarang(this.dataBarang).then(() => {
+              this.dataSharingService.refresh();
+              this.showAlert('Berhasil!', 'Progress barang telah diubah');
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  goToEditBarang() {
+    this.popoverCtrl.dismiss();
+    this.showAlert('Error!', 'Fitur ini belum tersedia');
+  }
+}
