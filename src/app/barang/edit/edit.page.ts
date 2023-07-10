cc5c0d8024b90779124e54eb68616c95a0561cf2
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-11 02:49:05 +0700

    edit barang is work

---
+++ src/app/barang/edit/edit.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataSharingService } from 'src/services/Database/data-sharing.service';
+import { DatabaseService } from 'src/services/Database/database.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.page.html',
+  styleUrls: ['./edit.page.scss'],
+})
+export class EditPage implements OnInit {
+  id: any = this.route.snapshot.paramMap.get('id');
+  dataBarang: any = [];
+  nama_barang: any;
+  kategori: any;
+  kategori_lainnya: any;
+  status: any;
+  extend_status: any;
+  jumlah_barang: any;
+  letak_barang: any;
+  keterangan: any;
+  jadwal_rencana: any;
+  jadwal_notifikasi: any;
+  reminder: any;
+
+  constructor(private databaseService: DatabaseService,
+    private dataSharingService: DataSharingService,
+    private router: Router,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.databaseService.getBarangById(this.id).then((data) => {
+      this.dataBarang = data;
+      this.nama_barang = data.nama_barang;
+      this.kategori = data.kategori !== 'Opsi Lainnya' ? data.kategori : '';
+      this.kategori_lainnya = data.kategori;
+      this.status = data.status;
+      this.extend_status = data.extend_status;
+      this.jumlah_barang = data.jumlah_barang;
+      this.letak_barang = data.letak_barang;
+      this.keterangan = data.keterangan;
+      this.jadwal_rencana = data.jadwal_rencana;
+      this.jadwal_notifikasi = data.jadwal_notifikasi;
+    });
+  }
+
+  saveToDatabase() {
+    this.dataBarang.nama_barang = this.nama_barang;
+    this.dataBarang.kategori = this.kategori == 'Opsi Lainnya' ? this.kategori_lainnya : this.kategori;
+    this.dataBarang.status = this.status;
+    this.dataBarang.extend_status = this.status == 'Dibuang' ? 'ke ' : (this.status == 'Dibeli' ? 'di ' : 'kepada ') + this.extend_status;
+    this.dataBarang.jumlah_barang = this.jumlah_barang;
+    this.dataBarang.letak_barang = this.letak_barang;
+    this.dataBarang.keterangan = this.keterangan;
+    this.dataBarang.jadwal_rencana = this.jadwal_rencana;
+    this.dataBarang.jadwal_notifikasi = this.reminder == 'Jadwal Rencana' ? this.jadwal_rencana : this.jadwal_notifikasi;
+    this.databaseService.updateBarang(this.dataBarang).then(() => {
+      this.dataSharingService.refresh();
+      this.router.navigateByUrl(`/barang/show/${this.id}`);
+    });
+  }
+}
+
