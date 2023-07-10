dd95caa7cff6fb8df2d8f40a4ebdc6ad7ef86df8
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-10 18:32:13 +0700

    change the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./auth/login/login.module').then(m => m.LoginPageModule)
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./auth/login/login.module').then(m => m.LoginPageModule)
+  // },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
@@ -20,27 +20,31 @@ const routes: Routes = [
   },
   {
     path: 'edit-profil',
-    loadChildren: () => import('./edit-profil/edit-profil.module').then( m => m.EditProfilPageModule)
+    loadChildren: () => import('./edit-profil/edit-profil.module').then(m => m.EditProfilPageModule)
   },
   {
-    path: 'tambah-barang',
-    loadChildren: () => import('./tambah-barang/tambah-barang.module').then( m => m.TambahBarangPageModule)
+    path: 'barang/create',
+    loadChildren: () => import('./barang/create/create.module').then(m => m.TambahBarangPageModule)
   },
   {
     path: 'tambah-jasa',
-    loadChildren: () => import('./tambah-jasa/tambah-jasa.module').then( m => m.TambahJasaPageModule)
+    loadChildren: () => import('./tambah-jasa/tambah-jasa.module').then(m => m.TambahJasaPageModule)
   },
   {
-    path: 'show-barang',
-    loadChildren: () => import('./show-barang/show-barang.module').then( m => m.ShowBarangPageModule)
+    path: 'barang/show/:id',
+    loadChildren: () => import('./barang/show/show.module').then(m => m.ShowBarangPageModule)
   },
   {
     path: 'show-jasa',
-    loadChildren: () => import('./show-jasa/show-jasa.module').then( m => m.ShowJasaPageModule)
+    loadChildren: () => import('./show-jasa/show-jasa.module').then(m => m.ShowJasaPageModule)
   },
   {
     path: 'verif-lupa-pw',
-    loadChildren: () => import('./auth/verif-lupa-pw/verif-lupa-pw.module').then( m => m.VerifLupaPwPageModule)
+    loadChildren: () => import('./auth/verif-lupa-pw/verif-lupa-pw.module').then(m => m.VerifLupaPwPageModule)
+  },
+  {
+    path: 'barang/edit/:id',
+    loadChildren: () => import('./barang/edit/edit.module').then(m => m.EditPageModule)
   }
 ];
 @NgModule({
