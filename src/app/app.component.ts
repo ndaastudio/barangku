2d51073cf2196f6eb43ea0b1206dadf314d831eb
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-11 18:34:43 +0700

    add hardware back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 import { DatabaseService } from 'src/services/Database/database.service';
 
 @Component({
@@ -7,7 +10,15 @@ import { DatabaseService } from 'src/services/Database/database.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private database: DatabaseService) { }
+  constructor(private database: DatabaseService,
+    private platform: Platform,
+    private router: Router) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (this.router.url === '/' || this.router.url === '/tabs/tab1' || this.router.url === '/tabs/tab2' || this.router.url === '/tabs/tab3') {
+        App.exitApp();
+      }
+    });
+  }
 
   ngOnInit() {
     this.database.init();
