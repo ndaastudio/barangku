7371dc32f6b1da8f230886f239287579192e3dd9
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-29 18:05:53 +0700

    created helpers function

--- src/app/helpers/functions.ts
+++ src/app/helpers/functions.ts
@@ -1,7 +1,27 @@
+import { AlertController, LoadingController } from "@ionic/angular";
+
 export function formatDate(date: string) {
     const monthNames = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
     const year = date.substring(0, 4);
     const month = monthNames[parseInt(date.substring(5, 7)) - 1];
     const day = date.substring(8, 10);
     return `${day} ${month} ${year}`;
+}
+
+export async function showAlert(alertCtrl: AlertController, header: string, message: string) {
+    const alert = await alertCtrl.create({
+        header: header,
+        message: message,
+        buttons: [{
+            text: 'OK',
+        }]
+    });
+    await alert.present();
+}
+
+export async function showLoading(loadingCtrl: LoadingController, message: string) {
+    const loading = await loadingCtrl.create({
+        message: message,
+    });
+    await loading.present();
 }
