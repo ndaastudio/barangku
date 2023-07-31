c02ce21878135948786fe38dfd2498a7ad0ecd09
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-31 20:12:33 +0700

    created force update sytem integrated with api

--- src/app/guards/update.guard.ts
+++ src/app/guards/update.guard.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { App } from '@capacitor/app';
+import { APIService } from 'src/services/API/api.service';
+import { StorageService } from 'src/services/LocalStorage/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private apiService: APIService,
+    private storageService: StorageService) {
+  }
 
   canActivate(): Promise<boolean> {
     return this.isUpdate();
   }
 
   async isUpdate(): Promise<boolean> {
-    const currentVersion = parseFloat((await App.getInfo()).version);
-    const latestVersion = 1.0;
-    if (currentVersion < latestVersion) {
-      this.router.navigateByUrl('/update');
+    const getUpdate = await this.apiService.getLatestVersion();
+    const data = {
+      currentVersion: (await App.getInfo()).version,
+      latestVersion: getUpdate.data.latest_version,
+      urlUpdate: getUpdate.data.url_update
+    }
+    if (parseFloat(data.currentVersion) < parseFloat(data.latestVersion)) {
+      await this.storageService.set('update', data);
+      await this.router.navigateByUrl('/update');
       return false;
     }
     return true;
--- src/app/update/update.page.ts
+++ src/app/update/update.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/services/LocalStorage/storage.service';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.page.scss'],
 })
 export class UpdatePage implements OnInit {
+  user: any;
+  currentVersion: any;
+  latestVersion: any;
+  urlUpdate: any;
 
-  constructor() { }
+  constructor(private storageService: StorageService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const data = await this.storageService.get('update');
+    const profile = await this.storageService.get('profile');
+    this.currentVersion = data.currentVersion;
+    this.latestVersion = data.latestVersion;
+    this.urlUpdate = data.urlUpdate;
+    this.user = profile.nama;
+  }
+
+  goToUpdate() {
+    window.open(this.urlUpdate, '_system');
   }
 
 }
