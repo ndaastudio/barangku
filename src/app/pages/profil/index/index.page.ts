7d849f8d78297d35d4fb74a042752f752f2d24bd
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-08-17 14:53:39 +0700

    remove sync data, move to pindah perangkat

--- src/app/pages/profil/index/index.page.ts
+++ src/app/pages/profil/index/index.page.ts
@@ -3,10 +3,8 @@ import { Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { LocalStorageService } from 'src/app/services/Database/local-storage.service';
 import { LocalNotifService } from 'src/app/services/App/local-notif.service';
-import { formatDate, showAlert, showLoading } from '../../../helpers/functions';
+import { showAlert, showLoading } from '../../../helpers/functions';
 import { AuthService } from 'src/app/services/API/auth.service';
-import { SyncDataService } from 'src/app/services/App/sync-data.service';
-import { DataRefreshService } from 'src/app/services/Database/data-refresh.service';
 
 @Component({
   selector: 'app-index',
@@ -18,16 +16,13 @@ export class IndexPage implements OnInit {
   email: any;
   nomor_telepon: any;
   jenis_akun: any;
-  tanggal_sinkron: any;
 
   constructor(private router: Router,
     private alertCtrl: AlertController,
     private loadingCtrl: LoadingController,
     private localStorage: LocalStorageService,
     private auth: AuthService,
-    private notif: LocalNotifService,
-    private sync: SyncDataService,
-    private dataRefresh: DataRefreshService,) {
+    private notif: LocalNotifService,) {
   }
 
   async ngOnInit() {
@@ -36,51 +31,10 @@ export class IndexPage implements OnInit {
     this.email = profile.email;
     this.nomor_telepon = profile.nomor_telepon;
     this.jenis_akun = profile.jenis_akun;
-    this.tanggal_sinkron = formatDate(profile.tanggal_sinkron);
   }
 
   goToEditProfil() {
     this.router.navigateByUrl('/profil/edit');
-  }
-
-  async submitSinkronisasi() {
-    const alertSinkronData = await this.alertCtrl.create({
-      header: 'Sinkron Data',
-      message: 'Silahkan pilih opsi yang diinginkan untuk melakukan sinkronisasi data',
-      buttons: [{
-        text: 'Download',
-        handler: async () => {
-          await alertSinkronData.dismiss();
-          try {
-            await showLoading(this.loadingCtrl, 'Loading...');
-            await this.sync.updateDataLocal();
-            await this.loadingCtrl.dismiss();
-            await this.ngOnInit();
-            await showAlert(this.alertCtrl, 'Berhasil!', 'Data di aplikasi sudah sinkron dengan data di server');
-            this.dataRefresh.refresh();
-          } catch (error: any) {
-            await this.loadingCtrl.dismiss();
-            await showAlert(this.alertCtrl, 'Error!', error.error.message);
-          }
-        }
-      },
-      {
-        text: 'Upload',
-        handler: async () => {
-          await alertSinkronData.dismiss();
-          try {
-            await showLoading(this.loadingCtrl, 'Loading...');
-            await this.sync.updateDataServer();
-            await this.loadingCtrl.dismiss();
-            await showAlert(this.alertCtrl, 'Berhasil!', 'Data di server sudah sinkron dengan data di aplikasi');
-          } catch (error: any) {
-            await this.loadingCtrl.dismiss();
-            await showAlert(this.alertCtrl, 'Error!', error.error.message);
-          }
-        }
-      }]
-    });
-    await alertSinkronData.present();
   }
 
   async submitKeluar() {
@@ -112,5 +66,9 @@ export class IndexPage implements OnInit {
       }]
     });
     await alertKeluar.present();
+  }
+
+  goToPindahPerangkat() {
+    this.router.navigateByUrl('/pindah-perangkat');
   }
 }
