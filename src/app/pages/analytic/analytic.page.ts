edd06db5ccd4915104047b251fadb52022d13cfb
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-02-21 00:50:51 +0700

    fix: data refresh statistik barang

--- src/app/pages/analytic/analytic.page.ts
+++ src/app/pages/analytic/analytic.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { BarangService as SQLiteBarang } from 'src/app/services/Database/SQLite/barang.service';
 import { LocalStorageService } from 'src/app/services/Database/local-storage.service';
+import { DataRefreshService } from 'src/app/services/Database/data-refresh.service';
 
 @Component({
   selector: 'app-analytic',
@@ -11,31 +12,53 @@ import { LocalStorageService } from 'src/app/services/Database/local-storage.ser
 export class AnalyticPage implements OnInit {
   platform: any = null;
   chart: any;
+  labels: string[] = [];
+  data: number[] = [];
 
   constructor(
     private localStorage: LocalStorageService,
-    private sqliteBarang: SQLiteBarang
+    private sqliteBarang: SQLiteBarang,
+    private dataRefresh: DataRefreshService,
   ) { }
 
   async ngOnInit() {
     this.platform = await this.localStorage.get('os');
-    this.createChart();
+    await this.getDataChart();
+    await this.createChart();
+    this.dataRefresh.refreshedData.subscribe(async () => {
+      await this.chart.destroy();
+      await this.getDataChart();
+      await this.createChart();
+    });
   }
 
   async createChart() {
-    const results = await this.sqliteBarang.getKategoriAndCount();
-    const labels = results.map((result: any) => result.kategori);
-    const data = results.map((result: any) => result.jumlah);
     this.chart = new Chart('statistik-barang', {
       type: 'doughnut',
       data: {
-        labels: labels,
+        labels: this.labels,
         datasets: [{
           label: 'Jumlah Barang',
-          data: data,
+          data: this.data,
           hoverOffset: 4
         }]
       }
     });
+  }
+
+  async getDataChart() {
+    const results = await this.sqliteBarang.getKategoriAndCount();
+    this.labels = results.map((result: any) => result.kategori);
+    this.data = results.map((result: any) => result.jumlah);
+  }
+
+  async handleRefresh(event: any) {
+    await this.chart.destroy();
+    this.data = [];
+    this.labels = [];
+    setTimeout(async () => {
+      await event.target.complete();
+      await this.ngOnInit();
+    }, 1500);
   }
 }
