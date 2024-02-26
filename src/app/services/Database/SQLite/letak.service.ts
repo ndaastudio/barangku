75024bca66e016712a3600418b71eab49677c757
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-02-26 16:33:13 +0700

    feat: db service for letak barang

---
+++ src/app/services/Database/SQLite/letak.service.ts
@@ -0,0 +1,264 @@
+import { Injectable } from '@angular/core';
+import { SQLiteObject } from "@awesome-cordova-plugins/sqlite/ngx";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LetakService {
+
+  private db!: SQLiteObject;
+
+  constructor() { }
+
+  init(db: SQLiteObject) {
+    this.db = db;
+  }
+
+  public async createTable() {
+    try {
+      await this.db.executeSql(`CREATE TABLE IF NOT EXISTS letak_barang (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama_barang TEXT,
+            kategori TEXT,
+            kategori_lainnya TEXT DEFAULT NULL,
+            jumlah_barang TEXT,
+            letak_barang TEXT,
+            progress INTEGER DEFAULT 0
+        );`, []);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }
+
+  public async createTableGambar() {
+    try {
+      await this.db.executeSql(`CREATE TABLE IF NOT EXISTS gambar_letak_barang (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_letak_barang INTEGER,
+            gambar TEXT,
+            FOREIGN KEY (id_letak_barang) REFERENCES letak_barang (id)
+        );`, []);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }
+
+  public async createGambar(id: number, nama: string) {
+    try {
+      const sql = `INSERT INTO gambar_letak_barang (id_letak_barang, gambar) VALUES (?, ?);`;
+      const results = await this.db.executeSql(sql, [id, nama]);
+      return results.insertId;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async getGambarById(id: number) {
+    try {
+      const sql = `SELECT * FROM gambar_letak_barang WHERE id_letak_barang = ?;`;
+      const results = await this.db.executeSql(sql, [id]);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+
+  public async deleteGambarByName(fileName: string) {
+    try {
+      const sql = `DELETE FROM gambar_letak_barang WHERE gambar = ?;`;
+      await this.db.executeSql(sql, [fileName]);
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async create(data: any) {
+    try {
+      const sql = `INSERT INTO letak_barang (nama_barang, kategori, kategori_lainnya, jumlah_barang, letak_barang) VALUES (?, ?, ?, ?, ?);`;
+      const results = await this.db.executeSql(sql, [data.nama_barang, data.kategori, data.kategori_lainnya, data.jumlah_barang, data.letak_barang]);
+      return results.insertId;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async createWithCustomId(data: any) {
+    try {
+      const sql = `INSERT INTO letak_barang (id, nama_barang, kategori, kategori_lainnya, jumlah_barang, letak_barang, progress) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+      const results = await this.db.executeSql(sql, [data.id_letak_barang, data.nama_barang, data.kategori, data.kategori_lainnya, data.jumlah_barang, data.letak_barang, data.progress]);
+      return results;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async getAll() {
+    try {
+      const sql = `SELECT * FROM letak_barang ORDER BY id DESC;`;
+      const results = await this.db.executeSql(sql, []);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+
+  public async getAllGambar() {
+    try {
+      const sql = `SELECT * FROM gambar_letak_barang;`;
+      const results = await this.db.executeSql(sql, []);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+
+  public async getById(id: number) {
+    try {
+      const sql = `SELECT * FROM letak_barang WHERE id = ?;`;
+      const results = await this.db.executeSql(sql, [id]);
+      return results.rows.item(0);
+    } catch (error) {
+      alert(error);
+      return null;
+    }
+  }
+
+  public async getByKategori(kategori: string) {
+    try {
+      const sql = `SELECT * FROM letak_barang WHERE kategori = ?;`;
+      const results = await this.db.executeSql(sql, [kategori]);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return null;
+    }
+  }
+
+  public async update(data: any) {
+    try {
+      const sql = `UPDATE letak_barang SET nama_barang = ?, kategori = ?, kategori_lainnya = ?, jumlah_barang = ?, letak_barang = ?, progress = ? WHERE id = ?;`;
+      await this.db.executeSql(sql, [data.nama_barang, data.kategori, data.kategori_lainnya, data.jumlah_barang, data.letak_barang, data.progress, data.id]);
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async deleteById(id: number) {
+    try {
+      const sql = `DELETE FROM letak_barang WHERE id = ?;`;
+      await this.db.executeSql(sql, [id]);
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async deleteGambarById(id: number) {
+    try {
+      const sql = `DELETE FROM gambar_letak_barang WHERE id = ?;`;
+      await this.db.executeSql(sql, [id]);
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+
+  public async search(keyword: string) {
+    try {
+      const sql = `SELECT * FROM letak_barang WHERE nama_barang LIKE ? OR kategori LIKE ? OR kategori_lainnya LIKE ?;`;
+      const results = await this.db.executeSql(sql, ['%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%']);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+
+  public async multipleFilter(kategori?: string[], progress?: number) {
+    try {
+      if (!kategori) kategori = [];
+
+      let baseQuery = `SELECT * FROM barang `;
+      let whereClause = '';
+      let whereValues = [];
+
+      if (kategori.length > 0) {
+        whereClause += `kategori IN (${kategori.map(() => '?').join(', ')}) `;
+        whereValues.push(...kategori);
+      }
+
+      if (progress !== null) {
+        if (whereClause.length > 0) {
+          whereClause += `AND progress = ? `;
+        } else {
+          whereClause += `progress = ? `;
+        }
+        whereValues.push(progress);
+      }
+
+      if (whereClause.length > 0) {
+        baseQuery += `WHERE ${whereClause} `;
+      }
+
+      const results = await this.db.executeSql(baseQuery, whereValues);
+
+      let data = [];
+
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+
+  public async getKategoriAndCount() {
+    try {
+      const sql = `SELECT kategori, COUNT(*) as jumlah FROM letak_barang GROUP BY kategori;`;
+      const results = await this.db.executeSql(sql, []);
+      let data = [];
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  }
+}
